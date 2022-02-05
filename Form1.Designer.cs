3cd599f1046aa943ea4634cebf846f56cb713977
Yusuf Fakhreddin <48655310+Yusuf-Fakhreddin@users.noreply.github.com>
2022-02-05 09:36:15 +0200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace MagicDates
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_day = new System.Windows.Forms.Label();
+            this.lbl_month = new System.Windows.Forms.Label();
+            this.lbl_sum = new System.Windows.Forms.Label();
+            this.lbl_year = new System.Windows.Forms.Label();
+            this.lbl_msg = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(16, 50);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 0;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Choose a Date";
+            // 
+            // lbl_day
+            // 
+            this.lbl_day.AutoSize = true;
+            this.lbl_day.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_day.Location = new System.Drawing.Point(250, 19);
+            this.lbl_day.Name = "lbl_day";
+            this.lbl_day.Size = new System.Drawing.Size(38, 18);
+            this.lbl_day.TabIndex = 2;
+            this.lbl_day.Text = "Day:";
+            // 
+            // lbl_month
+            // 
+            this.lbl_month.AutoSize = true;
+            this.lbl_month.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_month.Location = new System.Drawing.Point(339, 19);
+            this.lbl_month.Name = "lbl_month";
+            this.lbl_month.Size = new System.Drawing.Size(54, 18);
+            this.lbl_month.TabIndex = 3;
+            this.lbl_month.Text = "Month:";
+            // 
+            // lbl_sum
+            // 
+            this.lbl_sum.AutoSize = true;
+            this.lbl_sum.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_sum.Location = new System.Drawing.Point(445, 19);
+            this.lbl_sum.Name = "lbl_sum";
+            this.lbl_sum.Size = new System.Drawing.Size(43, 18);
+            this.lbl_sum.TabIndex = 4;
+            this.lbl_sum.Text = "Sum:";
+            // 
+            // lbl_year
+            // 
+            this.lbl_year.AutoSize = true;
+            this.lbl_year.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_year.Location = new System.Drawing.Point(445, 52);
+            this.lbl_year.Name = "lbl_year";
+            this.lbl_year.Size = new System.Drawing.Size(42, 18);
+            this.lbl_year.TabIndex = 5;
+            this.lbl_year.Text = "Year:";
+            // 
+            // lbl_msg
+            // 
+            this.lbl_msg.AutoSize = true;
+            this.lbl_msg.Font = new System.Drawing.Font("Times New Roman", 13F);
+            this.lbl_msg.Location = new System.Drawing.Point(249, 50);
+            this.lbl_msg.Name = "lbl_msg";
+            this.lbl_msg.Size = new System.Drawing.Size(122, 20);
+            this.lbl_msg.TabIndex = 6;
+            this.lbl_msg.Text = "Nothing Special";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(581, 165);
+            this.Controls.Add(this.lbl_msg);
+            this.Controls.Add(this.lbl_year);
+            this.Controls.Add(this.lbl_sum);
+            this.Controls.Add(this.lbl_month);
+            this.Controls.Add(this.lbl_day);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Name = "Form1";
+            this.Text = "Magic Date";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_day;
+        private System.Windows.Forms.Label lbl_month;
+        private System.Windows.Forms.Label lbl_sum;
+        private System.Windows.Forms.Label lbl_year;
+        private System.Windows.Forms.Label lbl_msg;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MagicDates
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            lbl_msg.Text = "";
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            DateTime chosenDate = dateTimePicker1.Value;
+
+            int day = chosenDate.Day;
+            int month = chosenDate.Month;  
+            int year = chosenDate.Year %100 ;
+
+            // check if if it's a magic date 
+            if (day + month == year)
+            {
+                lbl_msg.ForeColor = Color.Green;
+                lbl_msg.Text = "Magic Number!!";
+            }
+            else
+            {
+                lbl_msg.ForeColor = Color.Blue;
+
+                lbl_msg.Text = "Nothing Special";
+            }
+
+            // update all labels
+            lbl_day.Text = "Day: " + day;
+            lbl_month.Text = "Month: " + month;
+            lbl_year.Text = "Year: " + year;
+            lbl_sum.Text = "Sum: " + (day + month);
+
+        }
+    }
+}
